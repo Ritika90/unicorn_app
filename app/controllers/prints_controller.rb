f98165e78d982bb409a1edd2a5014dab142182b9
Ritika90 <erritika.1690@gmail.com>
2015-01-07 09:22:40 +0530

    cmt

--- app/controllers/prints_controller.rb
+++ app/controllers/prints_controller.rb
@@ -1,7 +1,7 @@
 class PrintsController < ApplicationController
   def index
     for i in 1..10000 do
-      sleep(10000)
+      # sleep(10000)
       puts "Hello, the number is #{i}"
     end
   end
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,6 +1,6 @@
 # config/unicorn.rb
 worker_processes 3
-timeout 3
+timeout 30
 preload_app true
 
 before_fork do |server, worker|
